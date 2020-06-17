464e9130bae5569813bf1d5f4557912b534e79c7
ulfahasanah <ulfa2912@outlook.com>
2020-06-18 00:03:05 +0700

    integer

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -15,4 +15,17 @@ for x in 1...3 {
 
 print("\nThe current value of welcomeMsg is \(welcomeMsg) and number is \(number)")
 
+/**
+ Integer adalah bilangan bulat tanpa komponen fraksional seperti 42 dan -32. Integer terdiri dari dua tipe, signed (positif, nol atau negatif) dan unsigned (positif atau nol).
+ */
+ 
+let minValueInt64 = Int64.min  // minValue sama  -9223372036854775808, dan bertipe Int64
+let maxValueInt64 = Int64.max  // maxValue sama dengan 9223372036854775807, dan bertipe Int64
+
+let minValueUInt32 = UInt32.min  // minValue sama dengan 0, dan bertipe UInt32
+let maxValueUInt32 = UInt32.max  // maxValue sama dengan 4294967295, dan bertipe UInt32
+
+print(minValueInt64, maxValueInt64, minValueUInt32, maxValueUInt32)
+
+
 
