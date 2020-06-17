f01e2893fb0ef09eebd7fe04e383dff08c5c2fe5
ulfahasanah <ulfa2912@outlook.com>
2020-06-17 23:20:31 +0700

    terminator

--- Variables.playground/Contents.swift
+++ Variables.playground/Contents.swift
@@ -10,8 +10,9 @@ for n in 1...3 {
     print(n)
 }
 for x in 1...3 {
-    print(x, terminator: "...")
+    print(x, terminator: ".")
 }
+
 print("\nThe current value of welcomeMsg is \(welcomeMsg) and number is \(number)")
 
 
